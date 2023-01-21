eb2e611b42d4bc581cc6a69437ec875b7a8b1f74
LuisPa Garcia <diganluispa@gmail.com>
2023-01-20 19:04:24 -0600

    chore: deleted console log of process.env

--- src/pages/save-text/saveText.utils.ts
+++ src/pages/save-text/saveText.utils.ts
@@ -1,7 +1,6 @@
 import { GistDB } from "gist-io";
 // import { GITHUB_TOKEN } from "./config.env";
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-console.log("process.env", process.env);
 
 const db = new GistDB({
   token: GITHUB_TOKEN,
